b5ba2bff8c0690d2820e4d5b927f7ad8a7955fca
Uwe Sauerbrei <uwe.sauerbrei@fincon.eu>
2017-12-01 10:05:26 +0100

    Setup Dialog
    + add file chooser
    + fixed layout

--- src/main/groovy/de/ostfale/beezle/control/ResourceService.groovy
+++ src/main/groovy/de/ostfale/beezle/control/ResourceService.groovy
@@ -11,6 +11,7 @@ class ResourceService {
     // font awesome icon aliases
     static final String ICON_EXIT = '\uf011'
     static final String ICON_DOWNLOAD = '\uf019'
+    static final String ICON_FILE = '\uf15b'
     static final String ICON_FOLDER = '\uf07b'
     static final String ICON_GITLAB = '\uf296'
     static final String ICON_INFO = '\uf129'
--- src/main/groovy/de/ostfale/beezle/control/SetupDialog.groovy
+++ src/main/groovy/de/ostfale/beezle/control/SetupDialog.groovy
@@ -1,5 +1,7 @@
 package de.ostfale.beezle.control
 
+import de.ostfale.beezle.AppConfig
+import groovy.util.logging.Slf4j
 import groovyx.javafx.SceneGraphBuilder
 import javafx.beans.value.ChangeListener
 import javafx.event.ActionEvent
@@ -7,14 +9,13 @@ import javafx.event.EventHandler
 import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.Scene
-import javafx.scene.control.Button
-import javafx.scene.control.Label
-import javafx.scene.control.PasswordField
-import javafx.scene.control.TextField
+import javafx.scene.control.*
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.GridPane
 import javafx.scene.layout.HBox
 import javafx.scene.layout.Priority
+import javafx.stage.DirectoryChooser
+import javafx.stage.FileChooser
 import javafx.stage.Modality
 import javafx.stage.Stage
 
@@ -27,6 +28,7 @@ import javafx.stage.Stage
  * </ul>
  * Created by usauerbrei on 30.11.2017
  */
+@Slf4j
 class SetupDialog implements MessageDialog {
 
     final Stage dlg
@@ -49,7 +51,7 @@ class SetupDialog implements MessageDialog {
         BorderPane borderPane = new BorderPane()
         borderPane.setPrefSize(600, 200)
         borderPane.setMargin(grid, new Insets(10))
-        borderPane.setMargin(btnBar, new Insets(5, 5, 5, 5))
+        borderPane.setMargin(btnBar, new Insets(5, 15, 5, 5))
         borderPane.setCenter(grid)
         borderPane.setBottom(createButtonBar())
         return new Scene(borderPane)
@@ -64,16 +66,26 @@ class SetupDialog implements MessageDialog {
         gpane.add(lblRepo, 0, 0)
         gpane.add(tfRepoDir = createTextField(), 1, 0)
         GridPane.setHgrow(tfRepoDir, Priority.ALWAYS)
+        gpane.add(createRepoButton(), 2, 0)
         gpane.add(lblKeyFile, 0, 1)
         gpane.add(tfKeyFileDir = createTextField(), 1, 1)
+        gpane.add(createKeyFileButton(), 2, 1)
         gpane.setGridLinesVisible(false)
         return gpane
     }
 
     private def createButtonBar() {
-        HBox hBox = new HBox(10, btnSave = createSaveButton(), createCancelButton())
+        HBox hBox = new HBox(15, btnSave = createSaveButton(), createCancelButton())
         hBox.setAlignment(Pos.CENTER_RIGHT)
         return hBox
+    }
+
+    private def createRepoButton() {
+        return new Button(font: ResourceService.FA14, text: ResourceService.ICON_FOLDER, tooltip: new Tooltip('Select local git repo'), onAction: showRepoFC)
+    }
+
+    private def createKeyFileButton() {
+        return new Button(font: ResourceService.FA14, text: ResourceService.ICON_FILE, tooltip: new Tooltip('Select SSH key file location'), onAction: showKeyFileFC)
     }
 
     private static def createSaveButton() {
@@ -115,5 +127,27 @@ class SetupDialog implements MessageDialog {
             }
         }
         btnSave.setDisable(disable)
+    }
+
+    def showRepoFC = {
+        DirectoryChooser dirDialog = new DirectoryChooser();
+        dirDialog.setTitle("Selecte local repository folder")
+        dirDialog.setInitialDirectory(new File("c:\\"))
+        File result = dirDialog.showDialog(dlg)
+        if (result) {
+            log.trace("Setup: Selected repository directory : ${result.getAbsolutePath()}")
+            tfRepoDir.setText(result.getAbsolutePath())
+        }
+    }
+
+    def showKeyFileFC = {
+        FileChooser fileChooser = new FileChooser()
+        fileChooser.setTitle("Select SSH key file")
+        fileChooser.setInitialDirectory(new File(AppConfig.USER_PROFILE))
+        File result = fileChooser.showOpenDialog(dlg)
+        if (result) {
+            log.trace("Setup: Selected SSH key file : ${result.getAbsolutePath()}")
+            tfKeyFileDir.setText(result.getAbsolutePath())
+        }
     }
 }
